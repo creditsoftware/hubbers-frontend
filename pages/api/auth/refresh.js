6e82e6c0fc7a7e957941a0a75ab7a0e4435a9a69
ccodelover <ase.naritoshi@bk.ru>
2021-07-06 14:02:29 +0200

    fix linkedin

--- pages/api/auth/refresh.js
+++ pages/api/auth/refresh.js
@@ -9,18 +9,26 @@ import {
 export default withSession(async (req, res) => {
   try {
     const token = await req.body;
-    const response = await axios.post(`${API.REFRESH_API}`, {}, {headers:`Bearer ${token}`});
+    const response = await axios.post(`${API.REFRESH_API}`, {}, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
     console.log(response.data);
-    if(response.data?.success){
-      req.session.set('refreshToken',response.data?.data.refreshToken);
-      req.session.set('accessToken',response.data?.data.accessToken);
-      req.session.set('user',response.data?.data.user);
+    if (response.data ?.success) {
+      req.session.set('refreshToken', response.data ?.data.refreshToken);
+      req.session.set('accessToken', response.data ?.data.accessToken);
+      req.session.set('user', response.data ?.data.user);
       await req.session.save();
-      res.status(200).json(response.data?.data.user);
+      res.status(200).json(response.data ?.data.user);
     }
   } catch (err) {
     console.log(err);
-    const { response: fetchResponse } = err;
-    res.status(fetchResponse?.status || 500).json(err.response?.data || {message: 'Failed to signin!'});
+    const {
+      response: fetchResponse
+    } = err;
+    res.status(fetchResponse ?.status || 500).json(err.response ?.data || {
+      message: 'Failed to signin!'
+    });
   }
 });
