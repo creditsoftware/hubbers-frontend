b5b0d8c43639aad5e818719beafe94912ae6fda4
ccodelover <ase.naritoshi@bk.ru>
2021-07-06 14:15:23 +0200

    fix linkedin

--- pages/api/auth/refresh.js
+++ pages/api/auth/refresh.js
@@ -14,7 +14,6 @@ export default withSession(async (req, res) => {
         Authorization: `Bearer ${token}`
       }
     });
-    console.log(response.data);
     if (response.data?.success) {
       req.session.set('refreshToken', response.data?.data.refreshToken);
       req.session.set('accessToken', response.data?.data.accessToken);
@@ -23,7 +22,6 @@ export default withSession(async (req, res) => {
       res.status(200).json(response.data?.data.user);
     }
   } catch (err) {
-    console.log(err);
     const {
       response: fetchResponse
     } = err;
