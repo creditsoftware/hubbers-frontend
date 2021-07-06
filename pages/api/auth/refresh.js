1c7c523bf038a2f7e7d188fd4b2d3327c9c6321f
ccodelover <ase.naritoshi@bk.ru>
2021-07-06 13:57:29 +0200

    fix linkedin

--- pages/api/auth/refresh.js
+++ pages/api/auth/refresh.js
@@ -12,13 +12,14 @@ export default withSession(async (req, res) => {
     const response = await axios.post(`${API.REFRESH_API}`, null, {headers:`Bearer ${token}`});
     console.log(response.data);
     if(response.data?.success){
-      req.session.set('refreshToken',response.data?.res.refreshToken);
-      req.session.set('accessToken',response.data?.res.accessToken);
-      req.session.set('user',response.data?.res.user);
+      req.session.set('refreshToken',response.data?.data.refreshToken);
+      req.session.set('accessToken',response.data?.data.accessToken);
+      req.session.set('user',response.data?.data.user);
       await req.session.save();
-      res.status(200).json(response.data?.res.user);
+      res.status(200).json(response.data?.data.user);
     }
   } catch (err) {
+    console.log(err);
     const { response: fetchResponse } = err;
     res.status(fetchResponse?.status || 500).json(err.response?.data || {message: 'Failed to signin!'});
   }
