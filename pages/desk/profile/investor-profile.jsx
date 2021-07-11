e9e76f243f9bebce52af57c95b1701ef15c76f5b
unknown <ase.naritoshi@bk.ru>
2021-07-10 20:58:03 -0700

    user profile design

---
+++ pages/desk/profile/investor-profile.jsx
@@ -0,0 +1,185 @@
+import React from 'react';
+import Link from 'next/link';
+import { PlusOutlined } from '@ant-design/icons';
+import { DeskPageHoc } from '../../../containers/hocs/DeskPageHoc';
+import { withSession } from '../../../utils/withSession';
+import { API } from '../../../constants/index';
+import useSWR from 'swr';
+import { fetcher } from '../../../utils/fetcher';
+import { MainProfile } from '../../../components/profile';
+import { Container } from '../../../components/Container';
+import { Input, Select, Radio, Slider, InputNumber, Space, Image, Checkbox, Avatar, Row, Col, Button } from 'antd';
+import { useState } from 'react';
+const { Option } = Select;
+const { TextArea } = Input;
+
+const InvestorProfile = ({ ...props }) => {
+  const { data } = useSWR(API.GET_USER_FROM_SESSIOM_API, fetcher, { initialData: props.auth });
+  const design = ['Logo', 'UX', 'Industrial design'];
+  const manufacturing = ['Product followup', 'Industrializaion'];
+  const [valuePeriod, setValuePeriod] = useState("now");
+  const [valueTeam, setValueTeam] = useState(true);
+  const [valueExpert, setValueExpert] = useState([]);
+  const [valueProduct, setValueProduct] = useState("not");
+  const [valueCategory, setValueCategory] = useState("product");
+  const [valueConsider, setValueConsider] = useState(false);
+  const [valueAmount, setValueAmount] = useState(50);
+  const [valueCountry, setValueCountry] = useState("all");
+  const [valueProject, setValueProject] = useState(true);
+  const [addState, setAddState] = useState(false);
+  const addStateChange = () => {
+    setAddState(true);
+  }
+  const cancelState = () =>{
+    setAddState(false);
+  }
+  const onChangeCategory = (e) =>{
+    setValueCategory(e.target.value);
+  }
+  const onChangeConsider = (e) =>{
+    setValueConsider(e.target.value);
+  }
+  const onChangeAmount = (e) =>{
+    setValueAmount(e);
+  }
+  const onChangeCountry = (e) => {
+    setValueCountry(e.target.value);
+  }
+  const onChangeProject = (e) => {
+    setValueProject(e.target.value);
+  }
+  return (
+    <DeskPageHoc title='Profile' activeSide={{ active: ['profile'], open: [] }} auth={{ ...data }}>
+      <React.Fragment>
+        <MainProfile />
+        <Container className="mt-4">
+          <React.Fragment>
+            <div>
+              <Link href="/desk/profile/">
+                <a style={{ display: 'inline-block' }} className="p-3">General Profile</a>
+              </Link>
+              <Link href="/desk/profile/creator-profile">
+                <a style={{ display: 'inline-block' }} className="p-3">Creator Profile</a>
+              </Link>
+              <Link href="/desk/profile/expert-profile">
+                <a style={{ display: 'inline-block' }} className="p-3">Expert</a>
+              </Link>
+              <Link href="/desk/profile/investor-profile">
+                <a style={{ display: 'inline-block' }} className="p-3 active-profile">Investor</a>
+              </Link>
+              <Link href="/desk/profile/hubbers-team">
+                <a style={{ display: 'inline-block' }} className="p-3">Hubbers Team</a>
+              </Link>
+            </div>
+            <div className="bg-white p-5">
+              <div className="max-w-50 m-auto">
+                <p className="fs-1 mt-2 mb-4">Hubbers community crowd funds great promising inventions, innovations and products, Would you consider contributing financially to a project that matches your interest?</p>
+                <Radio.Group onChange={onChangeConsider} value={valueConsider}>
+                  <Radio value={false}>No</Radio>
+                  <Radio value={true}>Yes (will enable you to receive regular offers based on your preference.)</Radio>
+                </Radio.Group>
+                <p className="fs-1 mt-5 mb-3">
+                  What categories of products, innovation and tech would you favor contributing to?
+                </p>
+                <Radio.Group onChange={onChangeCategory} value={valueCategory}>
+                  <Radio value="product">Product</Radio>
+                  <Radio value="innovation">Innovation</Radio>
+                  <Radio value="tech">Tech</Radio>
+                </Radio.Group>
+                <p className="fw-6 fs-2 my-4">
+                  Up to which amount would you consider to invest on project that match your interest?
+                </p>
+                <Row>
+                  <Col span={12}>
+                    <Slider
+                      min={50}
+                      max={2000}
+                      onChange={onChangeAmount}
+                      value={typeof valueAmount === 'number' ? valueAmount : 0}
+                    />
+                  </Col>
+                  <Col span={12}>
+                    &nbsp;&nbsp;&nbsp;More?
+                    <InputNumber
+                      min={50}
+                      max={2000}
+                      style={{ margin: '0 16px' }}
+                      value={valueAmount}
+                      onChange={onChangeAmount}
+                    />
+                    USD
+                  </Col>
+                </Row>
+                <p className="fs-1 mt-5 mb-3">
+                  Would you have any geographical preference
+                </p>
+                <Row>
+                  <Radio.Group onChange={onChangeCountry} value={valueCountry}>
+                    <Radio value="all">All</Radio>
+                    <Radio value="mycountry">My Country</Radio>
+                  </Radio.Group>
+                  <Select bordered={false} style={{ width: '100px', borderBottom: '1px solid grey' }}>
+                    <Option value="ch">China</Option>
+                    <Option value="in">India</Option>
+                    <Option value="ru">Russia</Option>
+                    <Option value="uk">Ukraine</Option>
+                  </Select>
+                </Row>
+                <p className="fs-1 mt-5 mb-3">
+                  Have you ever invested in promising project in the past [outside Hubbers community?
+                </p>
+                <Radio.Group onChange={onChangeProject} value={valueProject}>
+                  <Radio value={true}>Yes</Radio>
+                  <Radio value={false}>No</Radio>
+                </Radio.Group>
+                <Row className="mt-3 py-5">
+                  <button className="add-portfolio" onClick={addStateChange}>
+                    <PlusOutlined />
+                  </button>
+                </Row>
+                {addState ? (
+                  <Row>
+                    <Col span={10} className="d-flex fjc-center f-align-center">
+                      <button className="add-portfolio" onClick={addStateChange}>
+                        <PlusOutlined />
+                        <p className="mt-3">Add pic of project</p>
+                      </button>
+                    </Col>
+                    <Col span={14}>
+                      <Row className="mt-4 px-5">
+                        <Col span={24}>
+                          <label>Title</label>
+                          <input type="text" name="title" className="profile-input p-2 mt-1" style={{ borderBottom: '1px solid black', marginBottom: '24px' }} />
+                        </Col>
+                      </Row>
+                      <Row className="mt-4 px-5">
+                        <Col span={24}>
+                          <label>Description</label>
+                          <TextArea name="description" className="profile-input mt-3 p-2" style={{ borderBottom: '1px solid black', marginBottom: '24px' }} />
+                        </Col>
+                      </Row>
+                      <Row className="fjc-center">
+                        <Button type="hbs-outline-danger" size="large" shape="round" onClick={cancelState}>Cancel</Button>
+                        <Button type="hbs-primary" size="large" shape="round" className="ml-3">Add</Button>
+                      </Row>
+                    </Col>                    
+                  </Row>
+                ) : null}
+              </div>
+            </div>
+          </React.Fragment>
+        </Container>
+      </React.Fragment>
+    </DeskPageHoc>
+  );
+};
+export const getServerSideProps = withSession(async (ctx) => {
+  const { req } = ctx;
+  const user = await req.session.get('user');
+  if (user) {
+    return { props: { auth: { isLoggedIn: true, ...user } } };
+  } else {
+    return { props: { auth: { isLoggedIn: false } } };
+  }
+});
+export default InvestorProfile;
