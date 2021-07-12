01d6158a8c2c421655a34a299cb73cfd3a201fb7
ccodelover <ase.naritoshi@bk.ru>
2021-07-12 06:12:02 +0200

    .

--- pages/hubbers/who-we-are.jsx
+++ pages/hubbers/who-we-are.jsx
@@ -13,10 +13,37 @@ import { fetcher } from '../../utils/fetcher';
 const HubbersTeam = ({ ...props }) => {
   const settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   const [current, setCurrent] = React.useState([]);
   const [terminated, setTerminated] = React.useState([]);
