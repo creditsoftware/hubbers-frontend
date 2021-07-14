bcbb9d1841f4fec1f838886184c51e8aa6700019
unknown <ase.naritoshi@bk.ru>
2021-07-13 19:33:52 -0700

    put-your-skills, hbb-blockchain page design

--- pages/desk/put-your-skills.jsx
+++ pages/desk/put-your-skills.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import { DeskPageHoc } from '../../containers/hocs/DeskPageHoc';
 import { Container } from '../../components';
 import { withSession } from '../../utils/withSession';
 import { API } from '../../constants/index';
-import { Button, Space } from 'antd';
+import { Button, Space, Row, Col } from 'antd';
 import useSWR from 'swr';
 import { fetcher } from '../../utils/fetcher';
 const PutYourSkills = ({ ...props }) => {
@@ -24,21 +25,21 @@ const PutYourSkills = ({ ...props }) => {
       content: 'Find a job in existing job listing',
     }
   ];
-  // const jobTile = [
-  //   {
-  //     image: '/images/marketplace/specific-project.png',
-  //     content: 'Create a specific expertise based on your skills',
-  //   }, {
-  //     image: '/images/marketplace/specific-module.png',
-  //     content: 'Bring your expertise in a specific workspace [or product launcher] to help creators and companies complete it',
-  //   }, {
-  //     image: '/images/marketplace/work-per-hour.png',
-  //     content: 'Be looked up from your skills and work per hour',
-  //   }, {
-  //     image: '/images/marketplace/job-list.png',
-  //     content: 'Find a job in existing job listing',
-  //   }
-  // ];
+  const jobTile = [
+    {
+      image: '/images/marketplace/fundraising.png',
+      content: 'Fundraising',
+    }, {
+      image: '/images/marketplace/logistic.png',
+      content: 'Logistic',
+    }, {
+      image: '/images/marketplace/legal.png',
+      content: 'Legal',
+    }, {
+      image: '/images/marketplace/design.png',
+      content: 'Design',
+    }
+  ];
   return (
     <DeskPageHoc title='Activities' activeSide={{ active: ['activities'], open: [] }} auth={{ ...data }}>
       <Container className="py-5">
@@ -50,6 +51,34 @@ const PutYourSkills = ({ ...props }) => {
             <p className="fs-1 pt-5">Before jumping into describing your work, first fill better your expert profile only profile with more than 75% completion can offer their expertise.</p>
             <h1 className="fw-6 fs-2 pt-5">There are 4 ways to get a gig/job in Hubbers expert market place.</h1>
           </div>
+          <Row className="pt-4">
+            {
+              jobTile.map((item,index)=>{
+                return(
+                  <Col key={index} lg={6} sm={12} xs={24} className="p-3">
+                    <div
+                      className="general-card bg-white py-3"
+                      style={{
+                        height: '172px',
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: '100%',
+                          height: '100px',
+                          backgroundImage: `url(${item.image})`,
+                          backgroundRepeat: 'no-repeat',
+                          backgroundSize: 'contain',
+                          backgroundPosition: 'center'
+                        }}
+                      ></div>
+                      <div className="fs-2 fw-6 pt-3 text-center">{item.content}</div>
+                    </div>
+                  </Col>
+                );
+              })
+            }
+          </Row>
           <h1 className="text-center fw-6 fs-2 pt-5">There are 4 ways to get a gig/job in Hubbers expert market place.</h1>
           <Space size={70} wrap align="center" className="d-flex fjc-center py-5 mb-1">
             {
@@ -76,7 +105,11 @@ const PutYourSkills = ({ ...props }) => {
             <p className="fs-2 pt-5">Select the one you are more interested in and start working Want to have more details about how it works, know more here.</p>
             <p className="fs-2 pt-5">In order to provide our community with recognized experts, Hubbers implement a vetting system. Only experts that already got few reviews [or individual members] can apply for paid job.</p>
             <h1 className="fw-6 fs-2 pt-5">So to get your first review you need to provide few hours of works in your expertise for the community and get rewarded with HBB.</h1>
-            <Button type="hbs-primary" className="mt-5" shape="round" size="large">GET REWARDED WITH HBB</Button>
+            <Link href="/hubbers/hbb-hubbers-blockchain-community-token">
+              <a>
+                <Button type="hbs-primary" className="mt-5" shape="round" size="large">GET REWARDED WITH HBB</Button>
+              </a>
+            </Link>
           </div>
         </React.Fragment>
       </Container>
---
+++ pages/hubbers/hbb-hubbers-blockchain-community-token.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import Image from 'next/image';
+import { MainPageHoc } from '../../containers';
+import { Container } from '../../components';
+import { Row, Col } from 'antd';
+import { withSession } from '../../utils/withSession';
+import { API } from '../../constants/index';
+import useSWR from 'swr';
+import { fetcher } from '../../utils/fetcher';
+const ProductLauncher = ({ ...props }) => {
+  const { data } = useSWR(API.GET_USER_FROM_SESSIOM_API, fetcher, { initialData: props.auth });
+  const items1 = [{
+    image: '/images/hbb/bring_your_expertise.png',
+    text: 'Bringing your expertise In creators project Against HBB.'
+  },{
+    image: '/images/hbb/participating_in_product_competition.png',
+    text: 'Participating in product competition.'
+  },{
+    image: '/images/hbb/promoting_hubbers_activities.png',
+    text: 'Promoting Hubbers Activities & events.'
+  }];
+  const items2 = [{
+    image: '/images/hbb/get_usage_of_workspace.png',
+    text: 'Get usage of Hubbers workspace.'
+  },{
+    image: '/images/hbb/start-project.png',
+    text: 'Get Hubbers experts works for your product launch.'
+  },{
+    image: '/images/hbb/participate_in_hubbers_activities.png',
+    text: 'Participate in Hubbers premium activities and premium events.'
+  }];
+  return (
+    <MainPageHoc title='HBB Blockchain Taken' auth={{ ...data }}>
+      <React.Fragment>
+        <Container>
+          <div className="max-w-50 m-auto py-5 text-center">
+            <h1 className="fs-5 fw-6">Hubbers community token how it works</h1>
+            <p className="fs-1">As a purpose-driven community, Hubbers let member to bring their best ideas to create the pro user-friendly to the purpose Rewards in Hubbers are called HBB.</p>
+          </div>
+        </Container>
+        <div className="w-100 py-5 bg-white">
+          <Container>
+            <Row>
+              <Col span={24} className="text-center fs-2 fw-6">Rewards are given when YOU ARE</Col>
+              {
+                items1.map((item,index)=>{
+                  return(
+                    <Col key={index} lg={8} xs={24} className="p-5 pb-0 text-center">
+                      <Image width={230} height={150} src={item.image} />
+                      <p>{item.text}</p>
+                    </Col>
+                  );
+                })
+              }
+            </Row>
+          </Container>
+        </div>
+        <Container className="py-5">
+          <Row>
+            <Col span={24} className="fs-5 fw-6 text-center pb-5">You can also win HBB by doing a lot of useful things for Hubbers</Col>
+            <Col lg={12} sm={24} className="d-flex f-align-center">
+              <Image width={200} height={200} src="/images/hbb/post_in_your_social_media.png" />
+              <p className="px-3 fs-1 text-center">Post in your social media.</p>
+            </Col>
+            <Col lg={12} sm={24} className="d-flex fjc-end f-align-center">
+              <p className="px-3 fs-1 text-center">Bring new users that will use Hubbers.</p>
+              <Image width={200} height={200} src="/images/hbb/bring_new_users.png" />
+            </Col>
+          </Row>
+        </Container>
+        <div className="w-100 py-5 bg-white">
+          <Container>
+            <Row>
+              <Col span={24} className="text-center fs-2 fw-6">Rewards are given when YOU ARE</Col>
+              {
+                items2.map((item,index)=>{
+                  return(
+                    <Col key={index} lg={8} xs={24} className="p-5 pb-0 text-center">
+                      <Image width={230} height={150} src={item.image} />
+                      <p>{item.text}</p>
+                    </Col>
+                  );
+                })
+              }
+            </Row>
+          </Container>
+        </div>
+        <div
+          className="py-5 fc-white text-center"
+          style={{
+            backgroundImage: 'linear-gradient(0deg, #81A942 -7.92%, #B3DD5D 141.91%)'
+          }}
+        >
+          <Container>
+            <React.Fragment>
+              <p>When you reach a certain level of HBB, it is time to become part of our community and turn you HBB into HBS: blockchain-powered shares of Hubbers.</p>
+              <p>Ultimate HBS token: learn more soon</p>
+              <div className="m-auto bg-white p-2" style={{ width: 'max-content', borderRadius: '6px' }}>
+                <Image width={145} height={45} src="/images/logo/hubbers-logo.png" />
+              </div>
+            </React.Fragment>
+          </Container>
+        </div>
+      </React.Fragment>
+    </MainPageHoc>
+  );
+};
+export const getServerSideProps = withSession(async (ctx) => {
+  const {req} = ctx;
+  const user = await req.session.get('user');
+  if (user) {
+    return {props: {auth: {isLoggedIn: true, ...user } } };
+  } else {
+    return {props: {auth: {isLoggedIn: false } } };
+  }
+});
+export default ProductLauncher;
