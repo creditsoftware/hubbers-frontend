3234120121d809e4364f08670f66648acfdd15f1
ccodelover <ase.naritoshi@bk.ru>
2021-08-07 05:10:51 -0700

    grab-a-share page design, integrate community list in desk dashboad

--- pages/hubbers/grab-a-share.jsx
+++ pages/hubbers/grab-a-share.jsx
@@ -1,120 +1,140 @@
 import React from 'react';
-import Image from 'next/image';
-import { Row, Col, Button, Form, Input } from 'antd';
+import { Space } from 'antd';
+import { LinkedinFilled } from '@ant-design/icons';
 import { MainPageHoc } from '../../containers';
 import { Container, MainBanner } from '../../components';
 import { withSession } from '../../utils/withSession';
 import { API } from '../../constants/index';
 import { jwtDecode } from '../../utils/jwt';
 import useSWR from 'swr';
-import { fetcher } from '../../utils/fetcher';
+import { fetcher } from '../../utils/fetcher';import Avatar from 'antd/lib/avatar/avatar';
+import Link from 'next/link';
+1;
 const GrabAShare = ({ ...props }) => {
   const { data } = useSWR(API.GET_USER_FROM_SESSIOM_API, fetcher, { initialData: props.auth });
+  const investors = [{
+    avatar: 'https://hubbers-us.oss-us-west-1.aliyuncs.com/HJH6dR7r4.jpeg',
+    firstname: 'Benjamn',
+    lastname: 'Vignon',
+    country: 'France',
+    hbs: '5,100,000',
+    linkedin: true,
+  },{
+    avatar: 'https://hubbers-us.oss-us-west-1.aliyuncs.com/HJZrJ0GBV.jpeg',
+    firstname: 'Udit',
+    lastname: 'Veerwanie',
+    country: 'India',
+    hbs: '615,000',
+    linkedin: false,
+  },{
+    avatar: 'https://hubbers-us.oss-us-west-1.aliyuncs.com/HJH6dR7r4.jpeg',
+    firstname: 'Benjamn',
+    lastname: 'Vignon',
+    country: 'France',
+    hbs: '5,100,000',
+    linkedin: true,
+  },{
+    avatar: 'https://hubbers-us.oss-us-west-1.aliyuncs.com/HJZrJ0GBV.jpeg',
+    firstname: 'Udit',
+    lastname: 'Veerwanie',
+    country: 'India',
+    hbs: '615,000',
+    linkedin: false,
+  }];
+
   return (
     <MainPageHoc title="Grab a share" auth={{ ...data }}>
       <React.Fragment>
         <MainBanner
-          url='/images/requestinvestor_banner.png'
-          title={
-            <h1 className="fs-6 fw-6 fc-white">HUBBERS GRAB-A-SHARE</h1>
+          url='https://hubbers-hk.oss-cn-hongkong.aliyuncs.com/assets/home/banners/grab-a-share-banner.jpg'
+          title={ 
+            <h1 className="fs-6 fw-6 fc-white">SHARE & VALUE STATISTICS</h1>    
           }
           date={
-            <p className="fs-2 fw-5 fc-white">INVITATION ONLY</p>
+            <p className="fs-2 fw-5 fc-white">Find out our dynamics of growing<br/>hubbers business</p>
           }
         />
-        <Container>
+        <Container className="py-5">
           <React.Fragment>
-            <h1 className="text-center pt-5 fs-3 fw-6">How to qualify for Grab - a - Share</h1>
-            <Row className="py-4">
-              <Col sm={8} xs={24} className="text-center px-3">
-                <div className="grab-circle-img">
-                  <Image width={120} height={120} src="/images/heart_icon.png" />
-                </div>
-                <p className="fs-1 fw-5 pt-3">You love Hubbers, the hub of makers where innovative and creative consumer products are created.</p>
-              </Col>
-              <Col sm={8} xs={24} className="text-center px-3">
-                <div className="grab-circle-img">
-                  <Image width={120} height={120} src="/images/signin/become-a-super-expert.png" />
-                </div>
-                <p className="fs-1 fw-5 pt-3">You love Hubbers, the hub of makers where innovative and creative consumer products are created.</p>
-              </Col>
-              <Col sm={8} xs={24} className="text-center px-3">
-                <div className="grab-circle-img">
-                  <Image width={120} height={120} src="/images/signin/be-a-judge.png" />
-                </div>
-                <p className="fs-1 fw-5 pt-3">You love Hubbers, the hub of makers where innovative and creative consumer products are created.</p>
-              </Col>
-            </Row>
+            <h1 className="text-center fs-3 fw-6">HBS TOKEN VALUE</h1>
+            <div>
+              <label>HBS VALUE</label>
+              <p className="fs-3 fm-5">USD</p>
+              <label>NO. OF HBS I OWN</label>
+              <p className="fs-3 fm-5">10,000</p>
+              <label>CURRENT VALUE OF MY HBS TOKENS</label>
+              <p className="fs-3 fm-5">USD NaN</p>
+              <label>TOTAL NUMBER OF HBS TOKENS</label>
+              <p className="fs-3 fm-5">10,000,000</p>
+            </div>
           </React.Fragment>
         </Container>
-        <div className="text-center" style={{ backgroundColor: '#232323', textTransform: 'capitalize', color: 'white' }}>
+        <div className="pt-5 pb-5" style={{ backgroundColor: '#232323', textTransform: 'capitalize' }}>
           <Container>
             <React.Fragment>
-              <h1 className="text-center pt-5 fs-3 fw-6 fc-white">THE GRAB - A - SHARE DASHBOARD ALLOWS YOU TO:</h1>
-              <Row className="py-4">
-                <Col lg={8} xs={24} className="text-center p-3">
-                  <Image width={400} height={260} src="https://hubbers-hk.oss-cn-hongkong.aliyuncs.com/assets/home/tiles/become-a-creator.png" />
-                  <h1 className="pt-1 fs-3 fw-6 fc-white">Be part of a creative and inspiring founders community</h1>
-                </Col>
-                <Col lg={8} xs={24} className="text-center p-3">
-                  <Image width={400} height={260} src="https://hubbers-hk.oss-cn-hongkong.aliyuncs.com/assets/home/tiles/become-an-expert.png" />
-                  <h1 className="pt-1 fs-3 fw-6 fc-white">Stay informed about shares and be the first to acquire them</h1>
-                </Col>
-                <Col lg={8} xs={24} className="text-center p-3">
-                  <Image width={400} height={260} src="https://hubbers-hk.oss-cn-hongkong.aliyuncs.com/assets/home/tiles/become-an-investor.png" />
-                  <h1 className="pt-1 fs-3 fw-6 fc-white">Receive regular updates about Hubbers:</h1>
-                  <p className="mb-1 fs-1">existing shareholders</p>
-                  <p className="mb-1 fs-1">financial statements</p>
-                  <p className="mb-1 fs-1">receive regular company updates</p>
-                </Col>
-              </Row>
+              <h1 className="text-center pt-3 pb-5 fs-3 fw-6 fc-white">HUBBERS INVESTORS & TOKEN HOLDERS</h1>
+              <Space size={50} className="d-flex fjc-center pb-5" align="center" wrap>
+                {
+                  investors?.map((item,index)=>{
+                    return <div key={index} className="grab-holder bg-white">
+                      <Avatar size={170} src={item.avatar} />
+                      <h1 className="mt-4" title={`${item.firstname ? item.firstname : ''} ${item.lastname ? item.lastname : ''}`}>
+                        {`${item.firstname ? item.firstname : ''} ${item.lastname ? item.lastname : ''}`}
+                      </h1>
+                      <p className="fs-2 mb-1">{item.country}</p>
+                      <div className="d-flex fjc-space-between">
+                        <div className="text-left">
+                          <label>HBS</label>
+                          <p className="fs-2">{item.hbs}</p>
+                        </div>
+                        {
+                          item.linkedin && <Link href="#">
+                            <a>
+                              <LinkedinFilled className="fs-4" />
+                            </a>
+                          </Link>
+                        }
+                      </div>
+                    </div>;
+                  })
+                }
+              </Space>
             </React.Fragment>
           </Container>
         </div>
-        <Container className="text-center py-5">
-          <React.Fragment>
-            <h1 className="fs-5 fw-6">Access to Grab - a - Share dashboard:</h1>
-            <Row className="py-5">
-              <Col sm={12} xs={24} className="text-center px-3">
-                <div className="grab-circle-img">
-                  <Image width={100} height={100} src="/images/heart_icon.png" />
-                </div>
-                <p className="fs-2 fw-5 pt-3">Qualify through Hubbers directly via Linkedin.</p>
-                <a className="fs-2 fw-5" style={{ color: '#75AC2A' }}>Check out who we are</a>
-              </Col>
-              <Col sm={12} xs={24} className="text-center px-3">
-                <div className="grab-circle-img">
-                  <Image width={100} height={100} src="/images/signin/become-a-super-expert.png" />
-                </div>
-                <p className="fs-2 fw-5 pt-3">Ask a Hubbers shareholder to invite you.</p>
-                <p className="mb-1">Explain your motivation for becoming a Hubbers shareholder.Making a return on our stock shouldn&apos;t be the only reason.</p>
-              </Col>
-            </Row>
-            <Form
-              name='grabAShareForm'
-              onFinish={() => { }}
-              className='max-w-40 m-auto'
-            >
-              <Form.Item
-                name='email'
-                rules={[{ required: true, message: 'Please input the email!' }]}
-              >
-                <Input type='email' placeholder='Email' />
-              </Form.Item>
-              <Form.Item
-                name='name'
-              >
-                <Input type='text' placeholder='Name' />
-              </Form.Item>
-              <Form.Item
-                name='submit'
-                className='text-center'
-              >
-                <Button type="hbs-primary" htmlType='submit' size="large" shape="round">SEND NOW</Button>
-              </Form.Item>
-            </Form>
-          </React.Fragment>
-        </Container>
+        <div className="pt-5 pb-5" style={{ textTransform: 'capitalize' }}>
+          <Container>
+            <React.Fragment>
+              <h1 className="text-center pt-3 pb-5 fs-3 fw-6">HUBBERS OBSERVERS</h1>
+              <Space size={50} className="d-flex fjc-center pb-5" align="center" wrap>
+                {
+                  investors?.map((item,index)=>{
+                    return <div key={index} className="grab-holder" style={{ backgroundColor: 'lightgrey' }}>
+                      <Avatar size={170} src={item.avatar} />
+                      <h1 className="mt-4" title={`${item.firstname ? item.firstname : ''} ${item.lastname ? item.lastname : ''}`}>
+                        {`${item.firstname ? item.firstname : ''} ${item.lastname ? item.lastname : ''}`}
+                      </h1>
+                      <p className="fs-2 mb-1">{item.country}</p>
+                      <div className="d-flex fjc-space-between">
+                        <div className="text-left">
+                          <label>HBS</label>
+                          <p className="fs-2">{item.hbs}</p>
+                        </div>
+                        {
+                          item.linkedin && <Link href="#">
+                            <a>
+                              <LinkedinFilled className="fs-4" />
+                            </a>
+                          </Link>
+                        }
+                      </div>
+                    </div>;
+                  })
+                }
+              </Space>
+            </React.Fragment>
+          </Container>
+        </div>
       </React.Fragment>
     </MainPageHoc>
   );
